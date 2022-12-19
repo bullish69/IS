64bf170b40277436e227a81d5e3059e2dbf99293
ParthbBhut <parthbbhut1622@gmail.com>
2022-12-19 22:43:17 +0530

    Update StringRSA.py

--- StringRSA.py
+++ StringRSA.py
@@ -1,76 +1,57 @@
-import math
-
-# check if p and q are prime if not convert it into just closer prime number
+from math import gcd
 
 
-def make_prime(number, *exclude):
-    while True:
-        for i in range(2, number):
-            if number in exclude:
-                number += 1
-            if number % i == 0:
-                number += 1
+def lowestprime(n):
+    for i in range(n, 1, -1):
+        flag = 1
+        for j in range(2, i//2+1):
+            if i % j == 0:
+                flag = 0
                 break
-        if i == number-1:
-            break
-    return number
-
-# to get e which is relavtively prime to phi_n and less than phi_n
-
-
-def get_e(p, q, phi_n):
-    for i in range(2, phi_n):
-        if math.gcd(i, phi_n) == 1 and i != p and i != q:
+        if flag:
             return i
-
-# extended euclidean method to calculate d value
-
-
-def eem(phi_n, e):
-    r = [phi_n, e]
-    qt = ['-', '-']
-    x = [1, 0]
-    y = [0, 1]
-
-    while r[-1] != 1:
-        qt.append(r[-2] // r[-1])
-        r.append(r[-2] % r[-1])
-        x.append(x[-2] - qt[-1]*x[-1])
-        y.append(y[-2] - qt[-1]*y[-1])
-
-    print('r\tqt\tx\ty')
-    for i in range(len(r)):
-        print(f'{r[i]}\t{qt[i]}\t{x[i]}\t{y[i]}')
-
-    return y[-1] % phi_n
+    return 2
 
 
-# p and q are string input and pt is integer
-p = len(input("Enter p(str): "))
-q = len(input("Enter q(str): "))
-
-p = make_prime(p)
-q = make_prime(q, p)
-# p = 7
-# q = 11
-# e = 13
-print('The p value is:', p)
-print('The q value is:', q, '\n')
-
+x = input("Enter string 1 : ")
+y = input("Enter string 2 : ")
+p, q = lowestprime(len(x)), lowestprime(len(y))
+if p == q:
+    i = q+2
+    while True:
+        flag = 1
+        for j in range(2, i//2+1):
+            if i % j == 0:
+                flag = 0
+                break
+        if flag:
+            q = i
+            break
+        i += 1
+print(f"Value of p is {p}")
+print(f"Value of q is {q}")
 n = p*q
-phi_n = (p-1)*(q-1)
-e = get_e(p, q, phi_n)
-d = eem(phi_n, e)
-
-print()
-print('Public key:', (e, n))
-print('Private key:', (d, n))
-print()
-
-pt = int(input("Enter plain text(int): "))
-
-ct = pt**e % n
-print("After encryption\nThe cipher text is:", ct, '\n')
-
-pt = ct**d % n
-print('After decryption\nThe plain text is:', pt)
+phi = (p-1)*(q-1)
+i = 2
+while True:
+    if gcd(i, phi) == 1 and i != p and i != q:
+        e = i
+        break
+    i += 1
+i = 1
+while True:
+    if (i*e) % phi == 1 and i != e:
+        d = i
+        break
+    i += 1
+print(f"Value of e is {e}")
+print(f"Value of d is {d}")
+pt = int(input("Enter numerical plain text : "))
+if pt > phi:
+    pt = pt % phi
+    print(f"Since PT>phi so taking mod...")
+print(f"Final plain text is {pt}")
+ct = (pt**e) % n
+print(f"Cipher text is {ct}")
+decrypt = (ct**d) % n
+print(f"Decrypted text is {decrypt}")
